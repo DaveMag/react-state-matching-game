ae9e3c16b3273cb1c93110ad1ea3139202a6c570
SMD8 <davidemao86@gmail.com>
2022-08-09 20:19:50 +0200

    checking again

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,39 +19,39 @@ class App extends Component {
 
   handleTileClicked = (id, color) => {
     this.setState((state) => {
-      const tiles = state.tiles;
-      let toBeCleared = state.toBeCleared;
-      const selectedTileIndex = indexOfSelected(tiles, id, color);
-      let previousTileIndex = state.previousTileIndex;
+      const tiles = state.tiles
+      let toBeCleared = state.toBeCleared
+      const selectedTileIndex = indexOfSelected(tiles, id, color)
+      let previousTileIndex = state.previousTileIndex
 
       if (toBeCleared !== null) {
-        tiles[toBeCleared[0]].selected = false;
-        tiles[toBeCleared[1]].selected = false;
-        toBeCleared = null;
+        tiles[toBeCleared[0]].selected = false
+        tiles[toBeCleared[1]].selected = false
+        toBeCleared = null
       }
 
-      tiles[selectedTileIndex].selected = true;
+      tiles[selectedTileIndex].selected = true
 
       if (previousTileIndex !== null) {
-        const previousTile = tiles[previousTileIndex];
-        const selectedTile = tiles[selectedTileIndex];
+        const previousTile = tiles[previousTileIndex]
+        const selectedTile = tiles[selectedTileIndex]
 
         if (
           previousTile.id !== selectedTile.id &&
           previousTile.color === color
         ) {
-          selectedTile.matched = true;
-          previousTile.matched = true;
-          previousTileIndex = null;
+          selectedTile.matched = true
+          previousTile.matched = true
+          previousTileIndex = null
         } else {
-          toBeCleared = [previousTileIndex, selectedTileIndex];
-          previousTileIndex = null;
+          toBeCleared = [previousTileIndex, selectedTileIndex]
+          previousTileIndex = null
         }
       } else {
         previousTileIndex = selectedTileIndex;
       }
 
-      selectTile.selected = true;
+      selectTile.selected = true
 
       return {
         toBeCleared,
